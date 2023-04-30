17bc8acebdd932d6d2311e2f89d2e53d092f2822
maplejava0924 <maplements@gmail.com>
2023-04-30 05:27:30 +0000

    set invisibility and some refactoring

--- app/src/main/java/com/example/myapplication010/MainActivity.java
+++ app/src/main/java/com/example/myapplication010/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.myapplication010;
 
 import androidx.constraintlayout.widget.Guideline;
-import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.DragEvent;
@@ -47,20 +45,24 @@ public class MainActivity extends Activity{
         //背景画像をIDから検索しViewを変数に格納する
         ImageView background = findViewById(R.id.background);
 
-        //GuidelineをIDから検索しViewを変数に格納する。
-        //AndroidOSによって生成されたMainActivity.javaインスタンスの、Guideline myHandLineTop（インスタンス変数）に対してViewを格納する。
-        //myHandLineTopはクラス変数ではないのでインスタンス間で値が共有されないが、そもそもMainActivity.javaインスタンスは1つしか生成されない。
-        //当然MainActivity.javaインスタンス自体のGuideline myHandLineTopの値は書き換わっているのでメソッド内からthisで参照可能である。
-        //そこまで複雑なことをやりたいわけではないので、getterやsetterは不要でthisで参照する
+        /*
+          GuidelineをIDから検索しViewを変数に格納する。
+          AndroidOSによって生成されたMainActivity.javaインスタンスの、Guideline myHandLineTop（インスタンス変数）に対してViewを格納する。
+          myHandLineTopはクラス変数ではないのでインスタンス間で値が共有されないが、そもそもMainActivity.javaインスタンスは1つしか生成されない。
+          当然MainActivity.javaインスタンス自体のGuideline myHandLineTopの値は書き換わっているのでメソッド内からthisで参照可能である。
+          そこまで複雑なことをやりたいわけではないので、getterやsetterは不要でthisで参照する
+        */
         this.myHandLineTop = findViewById(R.id.myHandLineTop);
 
         //OnDragListenerインターフェースを実装した匿名クラス内で、Contextを使用するために、
         //匿名クラスの外でContextを定義しておく
         this.context = this;
 
-        //該当のImageViewに対してDragイベントが発生した時に呼ばれるリスナー
-        //Dropされるのはbackgroundの画像なので、backgroundに対してリスナーを定義する
-        //vはbackgroundのこと
+        /*
+          該当のImageViewに対してDragイベントが発生した時に呼ばれるリスナー
+          Dropされるのはbackgroundの画像なので、backgroundに対してリスナーを定義する
+          vはbackgroundのこと
+        */
         background.setOnDragListener((v,event) -> {
             switch (event.getAction()) {
                 // Dragが終了して、ViewにDropされたときの処理
@@ -90,7 +92,7 @@ public class MainActivity extends Activity{
                     case DragEvent.ACTION_DROP:
                         //Dropしたときに持っていたViewの情報を取得したいときはgetLocalState()を使用する
                         View droppedImageView = (View) event.getLocalState();
-                        enemyFieldDropEvent(v,droppedImageView, event);
+                        enemyFieldDropEvent(v,droppedImageView, event,true);
                         break;
                     default:
                         break;
@@ -98,20 +100,38 @@ public class MainActivity extends Activity{
                 return true;
             });
         }
+
+        //背景画像をIDから検索しViewを変数に格納する
+        ImageView enemyLeader = findViewById(R.id.enemyLeader);
+        enemyLeader.setOnDragListener((v,event) -> {
+            switch (event.getAction()) {
+                // Dragが終了して、ViewにDropされたときの処理
+                case DragEvent.ACTION_DROP:
+                    //Dropしたときに持っていたViewの情報を取得したいときはgetLocalState()を使用する
+                    View droppedImageView = (View) event.getLocalState();
+                    enemyFieldDropEvent(v,droppedImageView, event,false);
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        });
+
+
+
     }
 
     //ViewがTouchDownされたときのイベント
     //v:TouchDownされたView
     private void startDragView(View v){
-        //該当のImageViewの画像を取得し、nullでないならDrag状態にする
-        if (notNullImageViewSrc((ImageView) v)) {
-            // ViewをDrag状態にする
-            v.startDrag(null, new View.DragShadowBuilder(v), v, 0);
-            //ポップアップを出す
-            Toast.makeText(getApplicationContext(), "DragStart!", Toast.LENGTH_SHORT).show();
-            //logcatに情報を出力する
-            Log.i("debugMessage","Dragstart!");
-        }
+
+        // ViewをDrag状態にする
+        v.startDrag(null, new View.DragShadowBuilder(v), v, 0);
+        //ポップアップを出す
+        Toast.makeText(getApplicationContext(), "DragStart!", Toast.LENGTH_SHORT).show();
+        //logcatに情報を出力する
+        Log.i("debugMessage","DragStart!");
+
     }
 
     //backgroundImageViewがDropされたときのイベント
@@ -122,61 +142,49 @@ public class MainActivity extends Activity{
         //手を離した位置を格納する
         float dropY = event.getY();
         //Dropしたときに持っていたViewのIDがmyHandのいずれかであり、手を離した位置がmyHandLineTopより下であるときのみ処理を行う
-        if (isInIDArray(droppedImageId,this.myHandIds) && dropY < this.myHandLineTop.getY()) {
+        if (contains(this.myHandIds, droppedImageId) && dropY < this.myHandLineTop.getY()) {
 
             ImageView myField1ImageView= findViewById(R.id.myField1);
 
-            Drawable fairyDrawable = ContextCompat.getDrawable(this.context, R.drawable.fairy_nonevolve);
-            myField1ImageView.setImageDrawable(fairyDrawable);
+            myField1ImageView.setVisibility(View.VISIBLE);
 
             //ポップアップを表示する
-            Toast.makeText(getApplicationContext(), "myHandDroped!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "myHandDropped!", Toast.LENGTH_SHORT).show();
             //logcatに情報を出力する
             Log.i("debugMessage","myHandDropped!");
 
             //play処理
-
         }
     }
 
     // v:DropされたView
     // droppedView:Dropしたときに持っていたView
-    private void enemyFieldDropEvent(View v,View droppedView,DragEvent event){
-        //DropされたImageViewの画像を取得し、nullでないならイベントを発生させる
-        if (notNullImageViewSrc((ImageView) v)) {
-            //Dropしたときに持っていたViewのIDを格納する
-            int droppedImageId = droppedView.getId();
+    // attackToFollower:攻撃先が相手フォロワーであればTrue,相手リーダーであればFalse
+    private void enemyFieldDropEvent(View v, View droppedView, DragEvent event, boolean attackToFollower){
 
-            //Dropしたときに持っていたViewのIDがmyFieldのいずれかであるときのみ処理を行う
-            if (isInIDArray(droppedImageId,this.myFieldIds)) {
-                //ポップアップを表示する
-                Toast.makeText(getApplicationContext(), "MyFieldDroped!", Toast.LENGTH_SHORT).show();
-                //logcatに情報を出力する
-                Log.i("debugMessage", "myFieldDropped!");
+        //Dropしたときに持っていたViewのIDを格納する
+        int droppedImageId = droppedView.getId();
 
+        //Dropしたときに持っていたViewのID(droppedImageId)がmyFieldのいずれかであるときのみ処理を行う
+        if (contains(this.myFieldIds, droppedImageId)) {
+            //ポップアップを表示する
+            Toast.makeText(getApplicationContext(), "MyFieldDropped!", Toast.LENGTH_SHORT).show();
+            //logcatに情報を出力する
+            Log.i("debugMessage", "myFieldDropped!");
+
+            if(attackToFollower){
                 //フォロワーへattack処理
-
+            }else{
+                //リーダーへattack処理
             }
-        }
-    }
 
-    //ImageViewのsrcが@nullでないならtrueを返す
-    //ImageViewに何も画像がセットされていない（＝@null）ということは、そこにカードが存在していないということを表す
-    private boolean notNullImageViewSrc(ImageView imageView){
-
-        Drawable imageViewDrawable =  imageView.getDrawable();
-        if(imageViewDrawable != null){
-            return true;
-        }
-        else {
-            return false;
         }
 
     }
 
     //IDを取得し、配列に格納する
     //指定するidはmyHand1,myHand2,myHand3のように連番である必要がある
-    //また、格納先のidArrayの配列の長さとxmlに定義された該当のidのViewの個数が一致していないとNullpointする可能性があるので注意すること
+    //また、格納先のidArrayの配列の長さとxmlに定義された該当のidのViewの個数が一致していないとNull-pointする可能性があるので注意すること
     private void setIdArray(int[] idArray, String id){
         for (int i = 0; i < idArray.length; i++) {
             idArray[i] = getResources().getIdentifier(id + (i + 1), "id", getPackageName());
@@ -206,7 +214,7 @@ public class MainActivity extends Activity{
     }
 
     //idがIDArrayの中でひとつでも一致したときTrueを返す
-    private boolean isInIDArray(int id,int[] idArray){
+    private boolean contains(int[] idArray, int id){
         for (int myId : idArray) {
             if (id == myId) {
                 return true;
